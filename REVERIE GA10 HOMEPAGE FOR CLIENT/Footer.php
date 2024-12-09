9b6e1bd99bf6d2358086fde0c641f7ef98a755f9
CURT Reodique <cjnr2023-7281-70524@bicol-u.edu.ph>
2024-12-09 21:35:35 +0800

    Create  Footer.php

---
+++ REVERIE GA10 HOMEPAGE FOR CLIENT/Footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> reverie415@gmail.com</p>
+         <p> <i class="fas fa-map-marker-alt"></i> Region 5, Bicol Phillippines </p>
+      </div>
+
+      <div class="box">
+         <h3>Follow us on:</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-tiktok"></i> Tiktok</a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>REVERIE</span> </p>
+
+</section>
