c26a88b4f30884a2b4f87bfe9f3def4452d31a16
CURT Reodique <cjnr2023-7281-70524@bicol-u.edu.ph>
2024-12-09 21:10:40 +0800

    Create home.php

---
+++ REVERIE GA5 VISITOR HOMEPAGE/home.php
@@ -0,0 +1,140 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+if(isset($_POST['add_to_cart'])){
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_POST['product_image'];
+   $product_quantity = $_POST['product_quantity'];
+
+   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+   if(mysqli_num_rows($check_cart_numbers) > 0){
+      $message[] = 'already added to cart!';
+   }else{
+      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+      $message[] = 'product added to cart!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="home">
+   <video autoplay loop muted playsinline class="background-video">
+      <source src="images/home-bg.mp4.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+   </video>
+
+   <div class="content">
+      <h3>Your Beauty Journey Starts Here</h3>
+      <p>Confidence starts with self-care. Discover your best skin here with REVERIE</p>
+      <a href="about.php" class="white-btn">Discover More</a>
+   </div>
+
+</section>
+
+<section class="products">
+
+   <h1 class="title">Latest Products</h1>
+
+   <div class="box-container">
+
+      <?php  
+         $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
+         if(mysqli_num_rows($select_products) > 0){
+            while($fetch_products = mysqli_fetch_assoc($select_products)){
+      ?>
+     <form action="" method="post" class="box">
+      <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+      <div class="name"><?php echo $fetch_products['name']; ?></div>
+      <div class="price">₱<?php echo $fetch_products['price']; ?>.00</div>
+      <input type="number" min="1" name="product_quantity" value="1" class="qty">
+      <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+      <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+      <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+      <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+     </form>
+      <?php
+         }
+      }else{
+         echo '<p class="empty">no products added yet!</p>';
+      }
+      ?>
+   </div>
+
+   <div class="load-more" style="margin-top: 2rem; text-align:center">
+      <a href="shop.php" class="option-btn">Load More</a>
+   </div>
+
+</section>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/newLogo.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>About Us</h3>
+         <p>Are you ready to fall in love with your skin again?
+ Welcome to Reverie, where self-care meets accessible luxury. We're passionate about helping you reveal your most radiant self, inside and out.  We believe that self-care shouldn't be a chore – it should be a joy! Our carefully selected beauty products are designed to simplify your routine and elevate your self-care experience, making it both easy and enjoyable. We believe that everyone deserves to feel confident and beautiful in their own skin, and we're here to help you love it even more. Discover your best skin yet with Reverie.  Treat yourself, and rediscover the radiant you. </p>
+         <a href="about.php" class="btn">Read More</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="home-contact">
+
+   <div class="content">
+      <h3>Have any questions?</h3>
+      <p>Visit our page for more inquiries</p>
+      <a href="contact.php" class="white-btn">Contact Us</a>
+   </div>
+
+</section>
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
